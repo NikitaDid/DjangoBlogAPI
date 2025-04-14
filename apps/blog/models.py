59399cdef771cab934a140e0fe1d3ec396dd2486
NikitaDid <nikitadidenko222@gmail.com>
2025-04-14 17:53:15 +0200

    tag model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class BlogCategory(models.Model):
@@ -7,7 +8,6 @@ class BlogCategory(models.Model):
 
     def __str__(self):
         return self.name
-
 
     class Meta:
         verbose_name = 'Blog Category'
@@ -18,8 +18,9 @@ class Article(models.Model):
     category = models.ForeignKey(to=BlogCategory, on_delete=models.CASCADE)
     title = models.CharField(verbose_name='Title', max_length=255)
     text_preview = models.TextField(verbose_name='Text Preview', null=True, blank=True)
-    text = models.TextField(verbose_name='Text field')
+    text = models.TextField(verbose_name='Text Field')
     publish_date = models.DateTimeField(verbose_name='Publish Date')
+    tags = models.ManyToManyField(to='Tag', verbose_name='Tags', blank=True) #blank for admin panel. We can now save article without a tag
     updated_at = models.DateTimeField(verbose_name='Updated Date', auto_now=True)
     created_at = models.DateTimeField(verbose_name='Updated Date', auto_now_add=True)
 
@@ -29,3 +30,14 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Article'
         verbose_name_plural = 'Articles'
+
+
+class Tag(models.Model):
+    name = models.CharField(verbose_name='Tag Name', max_length=255)
+
+    def __str__(self):  # using to show it is in a normal way in admin
+        return self.name
+
+    class Meta:  # Human-readable singular name for admin panel
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
