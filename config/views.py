54c8a22e5ab1a1203a39b6fa68ce2040cf9d1309
NikitaDid <nikitadidenko222@gmail.com>
2025-04-24 18:50:43 +0200

    logo_and_main_page

--- config/views.py
+++ config/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. And you are on the main page.")
+    return render(request, 'index.html')
