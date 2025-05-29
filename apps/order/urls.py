31225283359c16fe2a9fba8abe932c5708ac7321
NikitaDid <nikitadidenko222@gmail.com>
2025-05-29 20:40:35 +0200

    cart

---
+++ apps/order/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from apps.order.views import add_to_cart_view, cart_view
+
+urlpatterns = [
+    path('add-to-cart/', add_to_cart_view, name='add_to_cart'),
+    path('cart/', cart_view, name='cart'),
+]
